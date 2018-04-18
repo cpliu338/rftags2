9c6b1f0c12780018abacc35d1f9d94cdd18d0fff
cpliu <cpliu338@yahoo.com>
2018-04-18 22:19:05 +0800

    20180418

--- src/main/java/wsd/m2/web/SampleBean.java
+++ src/main/java/wsd/m2/web/SampleBean.java
@@ -26,20 +26,28 @@ public class SampleBean implements Serializable {
     @javax.inject.Inject
     MongoBean mongoBean;
     
-    private int counter;
+    private int counter, counter3;
+    
+    private List<Integer[]> model;
     
     @PostConstruct
     public void init() {
         counter = 0;
+        model = new ArrayList<>();
+        for (int i=0; i<8; i++) {
+            Integer[] ir = new Integer[2];
+            ir[0] = i; ir[1] = i*2;
+            model.add(ir);
+        }
     }
     
     public void testFlow() {
-        throw new javax.faces.application.ViewExpiredException();
+        Logger.getLogger(SampleBean.class.getName()).log(Level.INFO, "Updating counter");
     }
     
     public void incCounter() {
-        counter++;
         Logger.getLogger(SampleBean.class.getName()).log(Level.INFO, "Updated counter {0}", counter);
+        counter3 = counter * 3;
     }
 
     /**
@@ -54,6 +62,27 @@ public class SampleBean implements Serializable {
      */
     public void setCounter(int counter) {
         this.counter = counter;
+    }
+
+    /**
+     * @return the model
+     */
+    public List<Integer[]> getModel() {
+        return model;
+    }
+
+    /**
+     * @return the counter3
+     */
+    public int getCounter3() {
+        return counter3;
+    }
+
+    /**
+     * @param counter3 the counter3 to set
+     */
+    public void setCounter3(int counter3) {
+        this.counter3 = counter3;
     }
 
 }
